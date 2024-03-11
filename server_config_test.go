a5019a6dc59cf8153cd4e5125ae90aa75a8eb758
ArrowInAKnee <gatexxl@gmail.com>
2024-03-11 14:15:31 +0300

    Update server config test to check errors

--- server_config_test.go
+++ server_config_test.go
@@ -6,42 +6,69 @@ import (
 	"testing"
 )
 
-type configCase struct {
-	name   string
-	source string
-	result *ServerConfig
-	err    error
-}
-
-var basic_case = configCase{
-	name: "Basic",
-	source: `
-server {
-	locations {
-		/test: files {
-			sources: /test/
-		}
-		/redirect: redirect {
-			target: /test
-		}
-	}
-}`,
-	result: &ServerConfig{
-		endpoints: []Endpoint{
-			{location: "/test", function: &EndpointFiles{"/test/"}},
-			{location: "/redirect", function: &EndpointRedirect{"/test"}},
-		},
-	},
-}
-
 func TestParseServerConfig(t *testing.T) {
-	// TODO: proper error checking
-	tests := []configCase{
-		basic_case,
+	type testCase struct {
+		name    string
+		source  string
+		result  *ServerConfig
+		wantErr bool
+	}
+
+	tests := []testCase{
+		{
+			name: "full_config",
+			source: `
+			server {
+				locations {
+					/test: files {
+						sources: /test/
+					}
+					/redirect: redirect {
+						target: /test
+					}
+				}
+			}`,
+			result: &ServerConfig{
+				endpoints: []Endpoint{
+					{location: "/test", function: &EndpointFiles{"/test/"}},
+					{location: "/redirect", function: &EndpointRedirect{"/test"}},
+				},
+			},
+			wantErr: false,
+		}, {
+			name: "missing_parameter",
+			source: `
+			server {
+				locations {
+					/test: files {
+						sources: 
+					}
+				}
+			}`,
+			wantErr: true,
+		}, {
+			name: "missing_parenthesis",
+			source: `
+			server {
+				locations {
+					/test: files {
+						sources: 
+				}
+			}`,
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got, err := ParseServerConfig(strings.NewReader(tt.source)); !reflect.DeepEqual(got, tt.result) || err != tt.err {
+			got, err := ParseServerConfig(strings.NewReader(tt.source))
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ParseServerConfig() error = \"%v\", wantErr %v", err, tt.wantErr)
+				return
+			}
+			if err != nil {
+				return
+			}
+			if !reflect.DeepEqual(got, tt.result) {
 				t.Errorf("ParseServerConfig() = %v, want %v", got, tt.result)
 			}
 		})
