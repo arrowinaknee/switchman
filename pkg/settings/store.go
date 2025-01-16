1d3eb1aa83f762890e316bb747a2f465351daef9
ArrowInAKnee <gatexxl@gmail.com>
2025-01-16 13:07:34 +0300

    settings: initial yaml implementation

--- pkg/settings/store.go
+++ pkg/settings/store.go
@@ -1,11 +1,16 @@
 package settings
 
 import (
+	"errors"
 	"fmt"
+	"os"
+	"path/filepath"
 	"reflect"
+	"strings"
 	"sync"
 
 	"github.com/mohae/deepcopy"
+	yaml "gopkg.in/yaml.v3"
 )
 
 var ErrNotFound = fmt.Errorf("settings: key not found")
@@ -17,14 +22,150 @@ type Store interface {
 
 type YamlStore struct {
 	Path string // path to directory with yaml files
+
 }
 
 func (y *YamlStore) Load(path string, s interface{}) error {
-	panic("unimplemented")
+	// TODO: provide a file lock
+	names := strings.Split(path, ".")
+	fname := names[0] + ".yaml"
+	bytes, err := os.ReadFile(filepath.Join(y.Path, fname))
+	if errors.Is(err, os.ErrNotExist) {
+		return errNotExist(path, err)
+	} else if err != nil {
+		return err
+	}
+	var root yaml.Node
+	if err := yaml.Unmarshal(bytes, &root); err != nil {
+		return err
+	}
+	rootMapping := root.Content[0]
+	node, err := findYamlNode(rootMapping, names[1:])
+	if err != nil {
+		return err
+	}
+	err = node.Decode(s)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (y *YamlStore) Save(path string, s interface{}) error {
-	panic("unimplemented")
+	// TODO: provide a file lock
+	names := strings.Split(path, ".")
+	fname := names[0] + ".yaml"
+	fullPath := filepath.Join(y.Path, fname)
+	bytes, err := os.ReadFile(fullPath)
+	var newRoot *yaml.Node
+	if errors.Is(err, os.ErrNotExist) {
+		newRoot, err = createYamlNode(names[1:], s)
+		if err != nil {
+			return err
+		}
+	} else {
+		if err != nil {
+			return err
+		}
+		var root yaml.Node
+		if err := yaml.Unmarshal(bytes, &root); err != nil {
+			return err
+		}
+		rootMapping := root.Content[0]
+		var err error
+		newRoot, err = alterYamlNode(rootMapping, names[1:], s)
+		if err != nil {
+			return err
+		}
+	}
+	out, err := yaml.Marshal(newRoot)
+	if err != nil {
+		return err
+	}
+	// FIXME: perms
+	// TODO: wrap error
+	return os.WriteFile(fullPath, out, 0644)
+}
+
+func findYamlNode(n *yaml.Node, names []string) (*yaml.Node, error) {
+	if len(names) == 0 {
+		return n, nil
+	}
+
+	if n.Kind != yaml.MappingNode {
+		// TODO: a custom error
+		return nil, fmt.Errorf("settings: %s: expected a mapping node, got Kind(%d)", names[0], n.Kind)
+	}
+
+	for i := 0; i < len(n.Content)-1; i += 2 {
+		if n.Content[i].Value == names[0] {
+			return findYamlNode(n.Content[i+1], names[1:])
+		}
+	}
+	return nil, ErrNotFound
+}
+
+func alterYamlNode(n *yaml.Node, names []string, value interface{}) (*yaml.Node, error) {
+	// TODO: move into own func
+	if len(names) == 0 {
+		newNode := &yaml.Node{}
+		err := newNode.Encode(value)
+		if err != nil {
+			return nil, err
+		}
+		return newNode, nil
+	}
+
+	if n == nil {
+		return createYamlNode(names, value)
+	}
+	if n.Kind != yaml.MappingNode {
+		return nil, fmt.Errorf("settings: %s: expected a mapping node, got Kind(%d)", names[0], n.Kind)
+	}
+
+	for i := 0; i < len(n.Content)-1; i += 2 {
+		if n.Content[i].Value == names[0] {
+			newNode, err := alterYamlNode(n.Content[i+1], names[1:], value)
+			if err != nil {
+				return nil, err
+			}
+
+			n.Content[i+1] = newNode
+			return n, nil
+		}
+	}
+
+	newNode, err := createYamlNode(names[1:], value)
+	if err != nil {
+		return nil, err
+	}
+	keyNode := &yaml.Node{}
+	keyNode.SetString(names[0])
+	n.Content = append(n.Content, keyNode, newNode)
+	return n, nil
+}
+
+func createYamlNode(names []string, value interface{}) (*yaml.Node, error) {
+	if len(names) == 0 {
+		newNode := &yaml.Node{}
+		err := newNode.Encode(value)
+		if err != nil {
+			return nil, err
+		}
+		return newNode, nil
+	}
+
+	nextNode, err := createYamlNode(names[1:], value)
+	if err != nil {
+		return nil, err
+	}
+	keyNode := &yaml.Node{}
+	keyNode.SetString(names[0])
+
+	mapping := &yaml.Node{}
+	mapping.Kind = yaml.MappingNode
+	mapping.Content = []*yaml.Node{keyNode, nextNode}
+	return mapping, nil
 }
 
 type VirtualStore struct {
--- pkg/settings/store_test.go
+++ pkg/settings/store_test.go
@@ -2,8 +2,13 @@ package settings
 
 import (
 	"errors"
+	"os"
+	"path/filepath"
 	"reflect"
+	"strings"
 	"testing"
+
+	"gopkg.in/yaml.v3"
 )
 
 type testSection struct {
@@ -93,5 +98,165 @@ func TestAllStores(t *testing.T) {
 				tt.test(t, store)
 			})
 		}
+	}
+}
+
+func TestYamlStore_Load(t *testing.T) {
+	type obj struct {
+		Name string `yaml:"name"`
+	}
+	files := map[string]string{
+		"config.yaml": `
+main:
+    first:
+        name: First nested
+    nested:
+        name: Nested value
+        unused: blah blah
+    another: value
+top:
+    name: Top Level`,
+		"second.yaml": `
+name: Second value`,
+	}
+
+	tests := []struct {
+		name    string
+		key     string
+		want    string
+		wantErr error
+	}{
+		{"topLevel", "config.top", "Top Level", nil},
+		{"nested", "config.main.nested", "Nested value", nil},
+		{"wholeFile", "second", "Second value", nil},
+		{"nofile", "nonexistent", "", ErrNotFound},
+		{"nokey", "config.nonexistent", "", ErrNotFound},
+		{"nokeyNested", "config.main.nonexistent", "", ErrNotFound},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// files will be removed automatically
+			dir := t.TempDir()
+			for name, text := range files {
+				err := os.WriteFile(filepath.Join(dir, name), []byte(text), 0644)
+				if err != nil {
+					t.Fatalf("WriteFile() err = %v", err)
+				}
+			}
+
+			store := &YamlStore{Path: dir}
+			var gotObj obj
+			err := store.Load(tt.key, &gotObj)
+			if !errors.Is(err, tt.wantErr) {
+				t.Errorf("Load() err = %q, want %q", err, tt.wantErr)
+			}
+			if err != nil {
+				return
+			}
+			if gotObj.Name != tt.want {
+				t.Errorf("Load() = %v, want %v", gotObj.Name, tt.want)
+			}
+		})
+	}
+}
+
+func TestYamlStore_Save(t *testing.T) {
+	type obj struct {
+		Name string `yaml:"name"`
+	}
+	fileData := map[string]interface{}{
+		"config": map[string]any{
+			"main": map[string]any{
+				"first": obj{
+					Name: "First nested",
+				},
+				"nested": obj{
+					Name: "Nested value",
+				},
+				"another": "value",
+			},
+			"top": obj{
+				Name: "Top Level",
+			},
+		},
+		"second": obj{
+			Name: "Second value",
+		},
+	}
+
+	tests := []struct {
+		name    string
+		key     string
+		value   string
+		wantErr error
+	}{
+		{"topLevel", "config.top", "New Value", nil},
+		{"nested", "config.main.nested", "New Value", nil},
+		{"wholeFile", "second", "New Value", nil},
+		{"newKey", "config.main.new", "New Value", nil},
+		{"newTree", "config.more.sub.deeper.new", "New Value", nil},
+		{"newFile", "third", "New Value", nil},
+		{"newFileNested", "third.main.new", "New Value", nil},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// files will be removed automatically
+			dir := t.TempDir()
+			for name, data := range fileData {
+				bytes, err := yaml.Marshal(data)
+				if err != nil {
+					t.Fatalf("Marshal() err = %v", err)
+				}
+				err = os.WriteFile(filepath.Join(dir, name+".yaml"), bytes, 0644)
+				if err != nil {
+					t.Fatalf("WriteFile() err = %v", err)
+				}
+			}
+
+			store := &YamlStore{Path: dir}
+			err := store.Save(tt.key, &obj{tt.value})
+			if !errors.Is(err, tt.wantErr) {
+				t.Errorf("Save() err = %v, want %v", err, tt.wantErr)
+			}
+			if err != nil {
+				return
+			}
+
+			var gotObj obj
+			err = store.Load(tt.key, &gotObj)
+			if err != nil {
+				t.Fatalf("Load(%q) err = %v", tt.key, err)
+			}
+			if gotObj.Name != tt.value {
+				t.Errorf("Load(%q) = %q, want %q", tt.key, gotObj.Name, tt.value)
+			}
+
+			var checkFiles func([]string, map[string]any)
+			checkFiles = func(curKey []string, data map[string]any) {
+				for k, v := range data {
+					if m, ok := v.(map[string]any); ok {
+						checkFiles(append(curKey, k), m)
+					} else if wantObj, ok := v.(obj); ok {
+						key := strings.Join(append(curKey, k), ".")
+						if key == tt.key {
+							continue
+						}
+						var gotObj obj
+						err := store.Load(key, &gotObj)
+						if err != nil {
+							t.Errorf("Load(%q) err = %v", key, err)
+							continue
+						}
+						if gotObj.Name != wantObj.Name {
+							t.Errorf("Load(%q) = %q, want %q", key, gotObj.Name, wantObj.Name)
+						}
+					}
+				}
+			}
+
+			checkFiles([]string{}, fileData)
+		})
 	}
 }
